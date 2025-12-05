062eed9e28c7fa0e36da8864928ef7717ed07bef
samuelmecham <142920339+samuelmecham@users.noreply.github.com>
2025-12-05 09:24:11 -0700

    Add files via upload

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+
+
+const awesomeFunction = (req, res) =>{
+    res.send("Hello world!");
+}
+
+const ttech = (req, res) =>{
+    res.send("Tooele Tech is Awesome!");
+}
+
+module.exports = { awesomeFunction, ttech };
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use("/", require("./routes"));
+
+
+
+app.listen(3000, () => {
+    console.log(`Test server running on port: ${PORT}`)
+});
