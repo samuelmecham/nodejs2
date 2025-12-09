44700d3b67b3987e3c064edbeb028cc4bf5dd890
samuelmecham <142920339+samuelmecham@users.noreply.github.com>
2025-12-09 08:18:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 app.use("/", require("./routes"));
 
@@ -8,4 +8,5 @@ app.use("/", require("./routes"));
 
 app.listen(3000, () => {
     console.log(`Test server running on port: ${PORT}`)
+
 });
